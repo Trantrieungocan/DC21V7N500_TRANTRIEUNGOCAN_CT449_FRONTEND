d720a35c537edb22e98078c20735078e3a95e8cc
Tran Trieu Ngoc An <andc21v7n500@vlvh.ctu.edu.vn>
2025-10-08 20:03:55 +0700

    Tao trang them lien he

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,32 +1,39 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 import ContactList from "@/components/ContactList.vue";
+
 const routes = [
-{
+  {
     path: "/",
     name: "contactbook",
     component: ContactBook,
-},
-{
+  },
+  {
     path: "/contacts",
     name: "contactlist",
     component: ContactList,
-},
-{
+  },
+  {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
-},
-{
+  },
+  {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true // Truyền các biến trong $route.params vào làm props
-},
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts/add",
+    name: "addcontact",
+    component: () => import("@/views/ContactAdd.vue"), // ✅ sửa ở đây
+  },
 ];
 
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
+
 export default router;
