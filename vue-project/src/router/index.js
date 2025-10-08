9d18cd66a4bef32e34a3faca776a0952d0b9db3a
Tran Trieu Ngoc An <andc21v7n500@vlvh.ctu.edu.vn>
2025-10-08 18:22:11 +0700

    Cap nhat csdl chi tiet hien thi danh sach cac contact lay ve tu server

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,12 +1,19 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import ContactList from "@/components/ContactList.vue";
 const routes = [
 {
-path: "/",
-name: "contactbook",
-component: ContactBook,
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+},
+{
+    path: "/contacts",
+    name: "contactlist",
+    component: ContactList,
 },
 ];
+
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
